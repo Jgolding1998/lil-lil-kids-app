d49f8948d9c583753e73bde357a03f98b4fa2b08
Jgolding1998 <justin.golding2@gmail.com>
2026-01-16 11:41:22 -0800

    Update sw.js: bump cache name to v2

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // cache on subsequent requests.  Additional resources are added to the
 // cache on demand via the fetch handler.
 
-const CACHE_NAME = 'lil-lil-kids-cache-v1';
+const CACHE_NAME = 'lil-lil-kids-cache-v2';
 
 // List of core files to pre‑cache.  These files are essential for
 // loading the app shell.  We include HTML, CSS, JS and icon.  The
